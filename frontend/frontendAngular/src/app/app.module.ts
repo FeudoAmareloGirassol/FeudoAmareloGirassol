ec2a84304eb7c7e8ba1ab1deb562af0a6bbfb16e
chaydson <chaydson14@gmail.com>
2022-03-28 19:40:15 -0300

    added styles to category carousel
    Co-authored-by: Pedro Henrique<pedrozinho.herique11@gmail.com>
    Co-authored-by: Ana Luiza<211030676@aluno.unb.br>
    Co-authored-by: Lucas Frazão <211031771@aluno.unb.br>
    Co-authored-by: Carolina Barbosa<carolinabarbosab09@gmail.com>

--- frontend/frontendAngular/src/app/app.module.ts
+++ frontend/frontendAngular/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/in-memory-data.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,20 +9,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageComponent } from './components/message/message.component';
 import { SearchComponent } from './components/search/search.component';
-import { CardsViewUserComponent } from './components/cards-view-user/cards-view-user.component'; // <-- NgModel lives here
-import { MatCardModule } from '@angular/material/card';
+import { CardsViewUserComponent } from './components/cards-view-user/cards-view-user.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterCompanyComponent } from './components/register-company/register-company.component';
 import { RegisterCustomerComponent } from './components/register-customer/register-customer.component';
 import { ViewUserComponent } from './components/view-user/view-user.component';
 import { SearchCardComponent } from './components/search-card/search-card.component';
+import{ CategoryCarouselComponent } from './components/category-carousel/category-carousel.component';
+
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { SearchCardComponent } from './components/search-card/search-card.compon
     RegisterCompanyComponent,
     RegisterCustomerComponent,
     ViewUserComponent,
-    SearchCardComponent
+    SearchCardComponent,
+    CategoryCarouselComponent,
   ],
   imports: [
     BrowserModule,
@@ -56,7 +58,8 @@ import { SearchCardComponent } from './components/search-card/search-card.compon
     MatInputModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]

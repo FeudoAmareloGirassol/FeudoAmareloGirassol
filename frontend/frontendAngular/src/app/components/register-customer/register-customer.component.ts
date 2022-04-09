c84d89649457562a939d89945a81cd2d43d68e61
pedrohenrique2077 <pedrozinho.herique11@gmail.com>
2022-04-09 17:13:47 -0300

    removed code you dont use

--- frontend/frontendAngular/src/app/components/register-customer/register-customer.component.ts
+++ frontend/frontendAngular/src/app/components/register-customer/register-customer.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from '../../services/message.service';
 import { CustomerModel, CustomerRequest } from '../../api/customer';
-// import { CustomerRegisterService } from '../../services/customer-register.service';
 import { Router } from '@angular/router';
 import { CustomerService } from 'src/app/services/customer.service';
 

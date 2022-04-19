6dad1aaf5afa8bdcd378ad189b1776e4fafdc162
LucasLopesFrazao <211031771@aluno.unb.br>
2022-04-19 17:32:29 -0300

    Validate CNPJ in backend and show message error in login
    Co-authored-by: Chaydson Ferreira <chaydson14@gmail.com>
    Co-authored-by: Carolina Barbosa <carolinabarbosab09@gmail.com>
    Co-authored-by: Pedro Henrique <pedrozinho.herique11@gmail.com>

--- backend/project/users/views.py
+++ backend/project/users/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework import filters
 from . import models, serializers
+import re
 
 
 class RegisterCompanyView(APIView):
@@ -21,6 +22,8 @@ class RegisterCompanyView(APIView):
         errors = {}
         if not userSerializer.is_valid():
             errors['user'] = userSerializer.errors
+        if not validate(request.data['company']['cnpj']):
+            errors['company'] = 'invalid cnpj'
         if not companySerializer.is_valid():
             errors['company'] = companySerializer.errors
         if errors:
@@ -93,3 +96,62 @@ class SchedulingViewSet(APIView):
 class UsersViewset(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.GetUsersSerializer
+
+
+# Validate CNPJ
+
+REGRESSIVES = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
+
+def validate(cnpj):
+    cnpj = only_numbers(cnpj)
+
+    try:
+        if its_sequence(cnpj):
+            return False
+    except:
+        return False
+
+    try:
+        new_cnpj = calculate_digit(cnpj=cnpj, digit=1)
+        new_cnpj = calculate_digit(cnpj=new_cnpj, digit=2)
+    except Exception as e:
+        return False
+
+    if new_cnpj == cnpj:
+        return True
+    else:
+        return False
+
+
+def calculate_digit(cnpj, digit):
+    if digit == 1:
+        regressives = REGRESSIVES[1:]
+        new_cnpj = cnpj[:-2]
+    elif digit == 2:
+        regressives = REGRESSIVES
+        new_cnpj = cnpj
+    else:
+        return None
+
+    total = 0
+    for indice, regressive in enumerate(regressives):
+        total += int(cnpj[indice]) * regressive
+
+    digit = 11 - (total % 11)
+    digit = digit if digit <= 9 else 0
+
+    return f'{new_cnpj}{digit}'
+
+
+def its_sequence(cnpj):
+    sequence = cnpj[0] * len(cnpj)
+
+    if sequence == cnpj:
+        return True
+    else:
+        return False
+
+
+def only_numbers(cnpj):
+    return re.sub(r'[^0-9]', '', cnpj)
